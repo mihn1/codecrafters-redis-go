d14c536d5155f35a68ec0d81628354b8fd6e8fa5
mihn <am141198@gmail.com>
2024-08-09 02:42:39 +0700

    respond to PING

--- app/server.go
+++ app/server.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	handleRequest(conn)
+}
+
+func handleRequest(conn net.Conn) {
+	fmt.Println("Received connection from: ", conn.RemoteAddr())
+
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading")
+		return
+	}
+	fmt.Println("Received message:", message)
+
+	res := "+PONG\r\n"
+	conn.Write([]byte(res))
 }
